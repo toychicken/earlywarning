42964a7c4d40f41ad0e95b0f58bde3a20c93db83
Leigh Garland <leigh.garland@johnlewis.co.uk>
2020-03-10 20:20:52 +0000

    Added essential '!'

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -3,7 +3,7 @@ let parseBody = arc.http.helpers.bodyParser;
 
 exports.handler = async function http(req) {
 	let body = parseBody(req);
-	let text = `END ${body.text}`;
+	let text = `END ${body.text}!`;
 	return {
 		headers: {'Content-Type': 'text/plain; charset=utf8'},
 		body: text,
