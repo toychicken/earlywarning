16f490ce2368848523b79759f5300e6d199d5ad8
Leigh Garland <leigh.garland@johnlewis.co.uk>
2020-03-10 09:40:46 +0000

    Updating POST endpoing - WOMM

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -1,9 +1,12 @@
+let arc = require('@architect/functions');
+let parseBody = arc.http.helpers.bodyParser;
 
-
-exports.handler = async function http (req) {
-  return {
-    statusCode: 200,
-    headers: {'content-type': 'text/plain'},
-    body: 'END JAMBO!'
-  }
-}
+exports.handler = async function http(req) {
+	let body = parseBody(req);
+	let text = `END ${body.text}`;
+	return {
+		headers: {'Content-Type': 'text/plain; charset=utf8'},
+		body: text,
+		isBase64Encoded: false,
+	}
+};
